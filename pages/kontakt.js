db87b3f185d77c70eb63cf29315d70c437d034c3
anhquan <anhquande@users.noreply.github.com>
2021-02-03 03:36:36 +0100

    init commit

---
+++ pages/kontakt.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import Layout from "../components/layout";
+
+export default function Kontakt(){
+  return (
+    <Layout>
+      <Head>
+        <title>Kontakt</title>
+      </Head>
+      <div>Kontakt</div>
+    </Layout>
+
+  )
+}
